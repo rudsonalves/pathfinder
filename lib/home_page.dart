9e6d4010520a798bb7a6276dcbe162c28648152a
Rudson R Alves <rudsonalves@yahoo.com.br>
2023-08-28 12:42:45 -0300

    ...

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,17 +2,6 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
-import 'models/ancestry_model.dart';
-import 'models/armor_model.dart';
-import 'models/classes_model.dart';
-import 'models/wp_simple_melee_model.dart';
-import 'models/wp_unnarmed_model.dart';
-import 'repository/ancestry_repository.dart';
-import 'repository/armors_repository.dart';
-import 'repository/classes_repository.dart';
-import 'repository/wp_simple_melee_repository.dart';
-import 'repository/wp_unnarmed_repository.dart';
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -21,86 +10,207 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void armorButton() async {
-    final armorsList = await ArmorsRepository.loadArmorsCSV();
-
-    for (final row in armorsList) {
-      ArmorModel armor = ArmorModel.fromList(row);
-      log(armor.toString());
-    }
-  }
-
-  void pressWpUnnarmedButton() async {
-    final wpUnnarmedList = await WpUnnarmedRepository.loadWpUnnarmedCSV();
-
-    for (final row in wpUnnarmedList) {
-      WpUnnarmedModel weapon = WpUnnarmedModel.fromList(row);
-      log(weapon.toString());
-    }
-  }
-
-  void pressWpSimpleMeleeButton() async {
-    final wpSimpleMeleeList =
-        await WpSimpleMeleeRepository.loadWpSimpleMeleeCSV();
-
-    for (final row in wpSimpleMeleeList) {
-      WpSimpleMeleeModel weapon = WpSimpleMeleeModel.fromList(row);
-      log(weapon.toString());
-    }
-  }
+  final playerNameController = TextEditingController();
+  final alignmentController = TextEditingController();
+  final playerController = TextEditingController();
+  final levelController = TextEditingController();
+  final experienceController = TextEditingController();
+  final deityController = TextEditingController();
+  final homelandController = TextEditingController();
+  final ancestryController = TextEditingController();
+  final heritageController = TextEditingController();
+  final classController = TextEditingController();
+  final sizeController = TextEditingController();
+  final genderController = TextEditingController();
+  final ageController = TextEditingController();
+  final heightController = TextEditingController();
+  final weightController = TextEditingController();
+  final eyesController = TextEditingController();
+  final hairController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pathfinder'),
+        title: const Text('Pathfinder 2'),
         centerTitle: true,
         elevation: 5,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 8,
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('filho 0'),
-            const Text('filho 1'),
-            const Text('filho 2'),
-            ElevatedButton(
-              onPressed: () async {
-                final listAncestry =
-                    await AncestryRepository.loadAncestryJson();
-
-                for (AncestryModel ancestry in listAncestry) {
-                  log(ancestry.toString());
-                }
-              },
-              child: const Text('Ancestry'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/PathFinder.png',
+                  scale: 2,
+                ),
+              ],
             ),
-            OutlinedButton(
-              onPressed: () async {
-                final listClasses = await ClassesRepository.loadClassesJson();
-
-                for (ClassesModel classe in listClasses) {
-                  log(classe.toString());
-                }
-              },
-              child: const Text('Classes'),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                CustomTextField(
+                  controller: playerNameController,
+                  labelText: 'Character Name',
+                  hintText: "Enter your Caracter's Name",
+                  flex: 2,
+                ),
+                const SizedBox(width: 8),
+                CustomTextField(
+                  controller: alignmentController,
+                  labelText: 'Alignment',
+                  hintText: 'True Neutral',
+                ),
+                const SizedBox(width: 8),
+                CustomTextField(
+                  controller: playerController,
+                  labelText: "Player's Name",
+                  hintText: 'Enter your Name',
+                  flex: 2,
+                ),
+              ],
             ),
-            TextButton(
-              onPressed: pressWpSimpleMeleeButton,
-              child: const Text('Botão 03'),
+            const SizedBox(
+              height: 16,
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                const Text('>>>'),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Botão 04'),
+                CustomTextField(
+                  controller: levelController,
+                  labelText: 'Level',
                 ),
-                const Text('<<<'),
+                const SizedBox(width: 16),
+                CustomTextField(
+                  controller: experienceController,
+                  labelText: 'Experience',
+                ),
+                const SizedBox(width: 16),
+                CustomTextField(
+                  controller: deityController,
+                  labelText: 'Deity',
+                ),
+                const SizedBox(width: 16),
+                CustomTextField(
+                  controller: homelandController,
+                  labelText: 'Homeland',
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(children: [
+              CustomTextField(
+                controller: ancestryController,
+                labelText: 'Ancestry',
+              ),
+              const SizedBox(width: 16),
+              CustomTextField(
+                controller: heritageController,
+                labelText: 'Heritage',
+              ),
+              const SizedBox(width: 16),
+              CustomTextField(
+                controller: classController,
+                labelText: 'Class',
+              ),
+              const SizedBox(width: 16),
+              CustomTextField(
+                controller: sizeController,
+                labelText: 'Size',
+              ),
+            ]),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                CustomTextField(
+                  controller: genderController,
+                  labelText: 'Gender',
+                ),
+                const SizedBox(width: 16),
+                CustomTextField(
+                  controller: ageController,
+                  labelText: 'Age',
+                ),
+                const SizedBox(width: 16),
+                CustomTextField(
+                  controller: heightController,
+                  labelText: 'Height',
+                ),
+                const SizedBox(width: 16),
+                CustomTextField(
+                  controller: weightController,
+                  labelText: 'Weight',
+                ),
+                const SizedBox(width: 16),
+                CustomTextField(
+                  controller: eyesController,
+                  labelText: 'Eyes',
+                ),
+                const SizedBox(width: 16),
+                CustomTextField(
+                  controller: hairController,
+                  labelText: 'Hair',
+                ),
+              ],
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    log(playerNameController.text);
+                    log(alignmentController.text);
+                    log(playerController.text);
+                  },
+                  child: const Text('Print'),
+                ),
               ],
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final String? hintText;
+  final TextEditingController? controller;
+  final int flex;
+
+  const CustomTextField({
+    super.key,
+    required this.labelText,
+    this.hintText,
+    this.controller,
+    this.flex = 1,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: flex,
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          labelText: labelText,
+          hintText: hintText,
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(16),
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/models/background_model.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+class BackgroundModel {
+  String name;
+  String description;
+  List<dynamic> boosts;
+  List<String> trained;
+  List<String> feat;
+
+  BackgroundModel({
+    required this.name,
+    required this.description,
+    required this.boosts,
+    required this.trained,
+    required this.feat,
+  });
+
+  @override
+  String toString() {
+    return 'BackgroundModel('
+        ' name: $name,\n'
+        ' description: $description,\n'
+        ' boosts: $boosts,\n'
+        ' trained: $trained,\n'
+        ' feat: $feat\n'
+        ')';
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'description': description,
+      'boosts': boosts,
+      'trained': trained,
+      'feat': feat,
+    };
+  }
+
+  factory BackgroundModel.fromMap(Map<String, dynamic> map) {
+    return BackgroundModel(
+      name: map['name'] as String,
+      description: map['description'] as String,
+      boosts: List<dynamic>.from(
+        (map['boosts'] as List<dynamic>).map(
+          (element) {
+            if (element is List) {
+              return element.map((e) => e as String).toList();
+            } else {
+              return element as String;
+            }
+          },
+        ),
+      ),
+      trained: List<String>.from(
+        (map['trained'] as List<dynamic>).map((element) => element as String),
+      ),
+      feat: List<String>.from(
+        (map['feat'] as List<dynamic>).map((element) => element as String),
+      ),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory BackgroundModel.fromJson(String source) =>
+      BackgroundModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
